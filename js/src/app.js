0cffec42d0548b757f416487a6e10260b28fe26c
ryanbmarx <ryanbmarx@gmail.com>
2017-03-24 16:02:00 -0500

    Ignore generated content

--- js/src/app.js
+++ js/src/app.js
@@ -1,2 +1,14 @@
 
 var boot = require('bootstrap.js');
+
+
+function toggleMobileNavMenu(){
+	console.log('menu!');
+		const mobileMenu = document.querySelector('.nav-buttons-wrapper');
+		console.log(mobileMenu);
+		mobileMenu.classList.toggle('nav-buttons-wrapper--visible')
+}
+
+window.onload = function(){
+
+}
