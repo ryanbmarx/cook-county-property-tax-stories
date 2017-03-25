2e6bc6578c86802132cbdd8972387a901313b564
ryanbmarx <ryanbmarx@gmail.com>
2017-03-24 19:04:36 -0500

    use a JS loop to activate each of the embeds

--- js/src/app.js
+++ js/src/app.js
@@ -1,16 +1,27 @@
 var pym = require('pym.js');
 var boot = require('bootstrap.js');
 
+// This allows iteration over an HTMLCollection (as I've done in setting the checkbutton event listeners,
+// as outlined in this Stack Overflow question: http://stackoverflow.com/questions/22754315/foreach-loop-for-htmlcollection-elements
+NodeList.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
+HTMLCollection.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
 
-function toggleMobileNavMenu(){
-	console.log('menu!');
-		const mobileMenu = document.querySelector('.nav-buttons-wrapper');
-		console.log(mobileMenu);
-		mobileMenu.classList.toggle('nav-buttons-wrapper--visible')
-}
 
 window.onload = function(){
-    var pymParent = new pym.Parent('lookup', 'http://graphics.chicagotribune.com/property-tax-assessments-map/lookup.html', {});
+
+    // Enable all the pym graphics by collecting all the containers, then 
+    // instantiating each one after plucking the necessary details from the 
+    // element's metadata
+    const pymContainers = document.querySelectorAll('.graphic-embed');
+    let pymParents = [];
+    for (var container of pymContainers){
+        console.log(container);
+        const   pymId = container.id,
+                pymUrl = container.dataset.iframeUrl;
+
+        let temp = new pym.Parent(pymId, pymUrl, {});
+        pymParents.push(temp);
+    }
 
     // Hide/show the mobile navigation menu
     document.getElementById('mobile-nav-toggle').addEventListener('click', function(e){
