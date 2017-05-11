a66029f84eafa032db7e0649a50b6b7aa5ca6c84
ryanbmarx <ryanbmarx@gmail.com>
2017-05-11 16:18:52 -0500

    remove unused code

--- js/src/app.js
+++ js/src/app.js
@@ -3,7 +3,7 @@ const pym = require('pym.js');
 // const boot = require('bootstrap.js');
 
 // http://iamdustan.com/smoothscroll/
-require('smoothscroll-polyfill').polyfill();
+// require('smoothscroll-polyfill').polyfill();
 
 // -------------------------------------------------------------------
 // THIS APP.JS ONLY IS FOR CODE NEEDED FOR ALL STORIES!
@@ -134,15 +134,15 @@ window.addEventListener('load', function() {
 
 // This make a smooth scroll between the charts and their methodologies (and back)
 // I'm using a polyfill found on npm
-const returnToChartLinks = document.querySelectorAll('.methodology__link')
-for (var link of returnToChartLinks){
-    link.addEventListener('click', function(e){
-        e.preventDefault();
-        const   scrollTarget = document.querySelector(`#${e.target.href.split('#')[1]}`),
-                box = scrollTarget.offsetParent.getBoundingClientRect();
-        let newTop = window.scrollY + box.top - 100; // this gets up past the navbar
-        window.scroll({ top: newTop, left: 0, behavior: 'smooth' });
+// const returnToChartLinks = document.querySelectorAll('.methodology__link')
+// for (var link of returnToChartLinks){
+//     link.addEventListener('click', function(e){
+//         e.preventDefault();
+//         const   scrollTarget = document.querySelector(`#${e.target.href.split('#')[1]}`),
+//                 box = scrollTarget.offsetParent.getBoundingClientRect();
+//         let newTop = window.scrollY + box.top - 100; // this gets up past the navbar
+//         window.scroll({ top: newTop, left: 0, behavior: 'smooth' });
 
 
-    })    
-}
+//     })    
+// }
