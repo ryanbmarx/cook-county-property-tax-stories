b5f72e880278090573541d269ca0f603e38f6723
ryanbmarx <ryanbmarx@gmail.com>
2017-06-01 18:50:22 -0500

    Use filter to make those &*^&*^*& smart quotes

--- tarbell_config.py
+++ tarbell_config.py
@@ -446,8 +446,30 @@ def strip_whitespace(text):
     Removes leading and trailing whitespace 
     """
     return text.strip()
+@blueprint.app_template_filter('dumb_to_smart_quotes')
+def dumb_to_smart_quotes(string):
+    """
+    Takes a string and returns it with dumb quotes, single and double,
+    replaced by smart quotes. Accounts for the possibility of HTML tags
+    within the string.
 
+    https://gist.github.com/davidtheclark/5521432
+    """
 
+    # Find dumb double quotes coming directly after letters or punctuation,
+    # and replace them with right double quotes.
+    string = re.sub(r'([a-zA-Z0-9.,?!;:\'\"])"', r'\1&#8221;', string)
+    # Find any remaining dumb double quotes and replace them with
+    # left double quotes.
+    string = string.replace('"', '&#8220;')
+    # Reverse: Find any SMART quotes that have been (mistakenly) placed around HTML
+    # attributes (following =) and replace them with dumb quotes.
+    string = re.sub(r'=&#8220;(.*?)&#8221;', r'="\1"', string)
+    # Follow the same process with dumb/smart single quotes
+    string = re.sub(r"([a-zA-Z0-9.,?!;:\"\'])'", r'\1&#8217;', string)
+    string = string.replace("'", '&#8216;')
+    string = re.sub(r'=&#8216;(.*?)&#8217;', r"='\1'", string)
+    return string
 
 # Google spreadsheet key
 SPREADSHEET_KEY = "1CDBifEOKDp5wc-uZjRDJlYTuiSvNE2pdbDNd2OPusyY"
